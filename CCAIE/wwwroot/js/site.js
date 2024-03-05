6bb01ae71530abf76917c638fcff419f5f99f41f
BrayanEstevezC <brayan_1321124024@uptecamac.edu.mx>
2024-03-05 03:03:51 -0600

    CCAIE_004

--- CCAIE/wwwroot/js/site.js
+++ CCAIE/wwwroot/js/site.js
@@ -4,8 +4,10 @@ window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     var nav = document.querySelector("a");
     var animation = getElement("animation");
+    const navegation = getElement("nav");
     header.classList.toggle("bg-white", window.scrollY > 0);
-    nav.classList.toggle("color", window.scrollY > 0);
+    navegation.classList.toggle("navbar-light", window.scrollY > 0);
+    navegation.classList.toggle("navbar-dark", window.scrollY < 1);
     animation.classList.toggle("fade-in-container", window.scrollY > 0);
 });
 
@@ -13,6 +15,8 @@ const icon = getElement("icon");
 const nav = getElement("nav");
 const iconClick = () => {
     nav.classList.toggle("bg-white");
+    nav.classList.toggle("navbar-light");
+    nav.classList.toggle("navbar-dark");
 }
 icon.onclick = iconClick;
 
