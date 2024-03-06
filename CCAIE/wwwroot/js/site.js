3425e108a9583d6c190776a17c3d96de58342084
BrayanEstevezC <brayan_1321124024@uptecamac.edu.mx>
2024-03-05 20:44:53 -0600

    CCIAE_005

--- CCAIE/wwwroot/js/site.js
+++ CCAIE/wwwroot/js/site.js
@@ -1,5 +1,14 @@
 ﻿var getElement = id => document.getElementById(id);
 var getElements = className => document.getElementsByClassName(className);
+
+const icon = getElement("icon");
+const nav = getElement("nav");
+const iconClick = () => {
+    nav.classList.toggle("bg-white");
+    nav.classList.toggle("text-dark");
+}
+icon.onclick = iconClick;
+
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     var nav = document.querySelector("a");
@@ -8,15 +17,15 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("bg-white", window.scrollY > 0);
     navegation.classList.toggle("navbar-light", window.scrollY > 0);
     navegation.classList.toggle("navbar-dark", window.scrollY < 1);
+    icon.classList.toggle("d-block", window.scrollY < 0);
+    icon.classList.toggle("d-none", window.scrollY < 1);
+    //icon.classList.add("d-none", window.scrollY < 1);
+
+    //icon.classList.remove("d-block", window.scrollY < 1);
+    //icon.classList.add("d-none", window.scrollY < 1);
+
     animation.classList.toggle("fade-in-container", window.scrollY > 0);
 });
 
-const icon = getElement("icon");
-const nav = getElement("nav");
-const iconClick = () => {
-    nav.classList.toggle("bg-white");
-    nav.classList.toggle("navbar-light");
-    nav.classList.toggle("navbar-dark");
-}
-icon.onclick = iconClick;
+
 
